320544730ed4522ca097785862e9352ad106a66f
Joshua Steele <joshuaericksteele@gmail.com>
2022-08-29 19:53:37 +1000

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
 
-function game() {
-   
-}
+
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -10,11 +8,31 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computersSelection) {
-    
-    
+    const playerChoice = prompt("Chose Rook, Paper, Scissors").textToLowercase;
+    if (playerSelection === computersSelection) {
+        return "Tied game!, play again!";
+    } if (playerSelection === "rock" && computersSelection === "scissors") {
+        return "You won! Rock smashes Scissors.";
+    } if (playerSelection === "scissors" && computersSelection === "paper") {
+        return "You won! Scissors cuts Paper.";
+    } if (playerSelection === "paper" && computersSelection === "rock") {
+        return "You won! Paper wraps Rock.";
+    } if (playerSelection === "scissors" && computersSelection === "rock") {
+        return "You lose! Rock smashes Scissors.";
+    } if (playerSelection === "paper" && computersSelection === "scissors") {
+        return "You lose! Scissors cuts Paper.";
+    } if (playerSelection === "rock" && computersSelection === "paper") {
+        return "You lose! Paper wraps Rock.";
+    }
 }
 
 // const playerSelection = "rock";
 let playerSelection = prompt("Chose rock paper or scissors");
 const computersSelection = getComputerChoice();
 console.log(computersSelection);
+
+// console.log(playRound("rock", "scissors"));
+
+function game() {
+   
+}
