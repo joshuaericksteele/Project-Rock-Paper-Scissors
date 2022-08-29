e027068455bdc0d9a98fb0cc39604fe884e8e1a0
Joshua Steele <joshuaericksteele@gmail.com>
2022-08-29 21:26:17 +1000

    main.js

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
-const choices = ["rock", "paper", "scissors"];
+let playersScore = 0
+let computersScore = 0
+  
 
 
 
 function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 
 function playRound(playerSelection, computersSelection) {
-    const playerChoice = prompt("Chose Rook, Paper, Scissors").textToLowercase;
+    
     if (playerSelection === computersSelection) {
         return "Tied game!, play again!";
-    } if (playerSelection === "rock" && computersSelection === "scissors") {
+    } else if (playerSelection === "rock" && computersSelection === "scissors") {
         return "You won! Rock smashes Scissors.";
-    } if (playerSelection === "scissors" && computersSelection === "paper") {
+    } else if (playerSelection === "scissors" && computersSelection === "paper") {
         return "You won! Scissors cuts Paper.";
-    } if (playerSelection === "paper" && computersSelection === "rock") {
+    } else if (playerSelection === "paper" && computersSelection === "rock") {
         return "You won! Paper wraps Rock.";
-    } if (playerSelection === "scissors" && computersSelection === "rock") {
+    } else if (playerSelection === "scissors" && computersSelection === "rock") {
         return "You lose! Rock smashes Scissors.";
-    } if (playerSelection === "paper" && computersSelection === "scissors") {
+    } else if (playerSelection === "paper" && computersSelection === "scissors") {
         return "You lose! Scissors cuts Paper.";
-    } if (playerSelection === "rock" && computersSelection === "paper") {
+    } else if (playerSelection === "rock" && computersSelection === "paper") {
         return "You lose! Paper wraps Rock.";
     }
 }
+    const playerSelection = prompt("Chose rock paper or scissors").toLowerCase()
+    const computersSelection = getComputerChoice();
 
-// const playerSelection = "rock";
-let playerSelection = prompt("Chose rock paper or scissors");
-const computersSelection = getComputerChoice();
-console.log(computersSelection);
+console.log(playRound(playerSelection, computersSelection));
+console.log(playerSelection, computersSelection);
 
-// console.log(playRound("rock", "scissors"));
+// function game() {
+//     for (let i = 0; i < 5; i++)
+    
+//     console.log(playRound(playerSelection, computersSelection));
+// }
+// // if (playersScore > computersScore) {
+// //     return "You beat the computer! You are a Star"
+// // }
+// // console.log(game);
 
-function game() {
-   
-}
+// console.log(game());
