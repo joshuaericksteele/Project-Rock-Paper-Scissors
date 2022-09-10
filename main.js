b6101c9efa855198ad6ca3173161f44ddcbb5e96
Joshua Steele <joshuaericksteele@gmail.com>
2022-09-10 23:53:54 +1000

    index.html

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const choiceBtns = document.querySelectorAll(".choiceBtns");
 const winText = document.querySelector("#winText");
 const lossText = document.querySelector("#lossText");
 const drawText = document.querySelector("#drawText");
+const gameText = document.querySelector("#gameText");
+
 let player;
 let computer;
 let result;
@@ -12,13 +14,25 @@ let win = 0;
 let loss = 0;
 let draw = 0;
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
 
+
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    
+   
+
+ 
     player = button.textContent;
     computer = computerTurn();
     playerText.textContent = `Player: ${player}`;
     computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
+    winText.textContent = `Wins: ${win}`;
+    lossText.textContent = `Losses: ${loss}`;
+    drawText.textContent = `Draws: ${draw}`;
+    gameText.textContent = game();
+
+
 }));
 
 function computerTurn() {
@@ -27,18 +41,49 @@ function computerTurn() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 function checkWinner() {
-
-    if (player == computer) {
-        return "Draw!";
-        
+    if (player === computer) {
+        draw++
+        return "Tied Game!, Play again!"
     }
-    else if (computer == "ROCK") {
-        return (player == "PAPER") ? "You Win !" : "You Lose!";
+    else if (player === "ROCK" && computer === "SCISSORS") {
+        win++
+        return "You WIN! Rock smashes Scissors!"
     }
-    else if (computer == "PAPER") {
-        return (player == "SCISSORS") ? "You Win !" : "You Lose!";
+    else if (player === "SCISSORS" && computer === "PAPER") {
+        win++
+        return "You WIN! Scissors cuts up Paper!"
     }
-    else if (computer == "SCISSORS") {
-        return (player == "ROCK") ? "You Win !" : "You Lose!";
+    else if (player === "PAPER" && computer === "ROCK") {
+        win++
+        return "You WIN! Paper wraps Rock!"
+    }
+    else if (player === "SCISSORS" && computer === "ROCK") {
+        loss++
+        return "You LOSE! Rock smashes Scissors!"
+    }
+    else if (player === "PAPER" && computer === "SCISSORS") {
+        loss++
+        return "You LOSE! Scissors cuts up Paper"
+    }
+    else if (player === "ROCK" && computer === "PAPER") {
+        loss++
+        return "You LOSE! Paper wraps Rock!"
     }
 }
+
+function game() {
+
+    if (win === 5) {
+        return "You beat the Computer! Well Done!"
+    }
+    else if (loss === 5) {
+        return "The Computer Won! Try again"
+    }
+    else if (draw === 5) {
+        return "Game tied! Play again!"
+    }
+
+}
+
+
+
