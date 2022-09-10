0c7e975a8cf9e4a1d6bf7d807a2ce34f7a60c0f6
Joshua Steele <joshuaericksteele@gmail.com>
2022-09-10 21:03:46 +1000

    index.html

--- main.js
+++ main.js
@@ -1,52 +1,44 @@
-let playersScore = 0
-let computersScore = 0
-  
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
+const computerText = document.querySelector("#computerText");
+const playerText = document.querySelector("#playerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtns");
+const winText = document.querySelector("#winText");
+const lossText = document.querySelector("#lossText");
+const drawText = document.querySelector("#drawText");
+let player;
+let computer;
+let result;
+let win = 0;
+let loss = 0;
+let draw = 0;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computer = computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+function computerTurn() {
+
+    const choices = ["ROCK", "PAPER", "SCISSORS"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
+function checkWinner() {
 
-function playRound(playerSelection, computersSelection) {
-    
-    if (playerSelection === computersSelection) {
-        return "Tied game!, play again!";
-    } else if (playerSelection === "rock" && computersSelection === "scissors") {
-        playersScore++
-        return "You won! Rock smashes Scissors.";
-    } else if (playerSelection === "scissors" && computersSelection === "paper") {
-        playersScore++
-        return "You won! Scissors cuts Paper.";
-    } else if (playerSelection === "paper" && computersSelection === "rock") {
-        playersScore++
-        return "You won! Paper wraps Rock.";
-    } else if (playerSelection === "scissors" && computersSelection === "rock") {
-        computersScore++
-        return "You lose! Rock smashes Scissors.";
-    } else if (playerSelection === "paper" && computersSelection === "scissors") {
-        computersScore++
-        return "You lose! Scissors cuts Paper.";
-    } else if (playerSelection === "rock" && computersSelection === "paper") {
-        computersScore++
-        return "You lose! Paper wraps Rock.";
+    if (player == computer) {
+        return "Draw!";
+        
+    }
+    else if (computer == "ROCK") {
+        return (player == "PAPER") ? "You Win !" : "You Lose!";
+    }
+    else if (computer == "PAPER") {
+        return (player == "SCISSORS") ? "You Win !" : "You Lose!";
+    }
+    else if (computer == "SCISSORS") {
+        return (player == "ROCK") ? "You Win !" : "You Lose!";
     }
 }
-
-function game() {
-    // for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Chose rock paper or scissors", "Rock, Paper or Scissors").toLowerCase();
-    const computersSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computersSelection));
-    console.log(playerSelection, computersSelection);
-    // }
-    if (playersScore > computersScore) {
-        return "You beat the computer! You are a Star";
-    } else if (playersScore < computersScore) {
-        return "The computer won! Try again";
-    } else if (playersScore === computersScore) {
-        return "Game is tied";
-    }
-}
-
-    game();
-
-// console.log(game());
